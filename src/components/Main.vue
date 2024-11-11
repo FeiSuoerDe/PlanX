d3c17aba49b4d206c55ccfca44f126ed678538a4
fi <2153602387@qq.com>
2024-11-11 17:01:43 +0800

    基本框架搭建

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,88 @@
 <template>
     <v-app>
-        <v-btn @click="toggleTheme">toggle theme</v-btn>
+        <v-btn @click="toggleTheme" style="position: fixed;left: 10px;top: 10px;">toggle theme</v-btn>
+        <v-container style="max-width: 500px ;margin: 0 auto;margin-top: 200px;">
+            <v-text-field v-model="newTask" label="What are you working on?" variant="solo" @keydown.enter="create">
+                <template v-slot:append-inner>
+                    <v-fade-transition>
+                        <v-btn v-show="newTask" icon="mdi-plus-circle" variant="text" @click="create"></v-btn>
+                    </v-fade-transition>
+                </template>
+            </v-text-field>
+            <h2 class="text-h4 text-success ps-4"> Tasks:&nbsp; <v-fade-transition leave-absolute>
+                    <span :key="`tasks-${tasks.length}`"> {{ tasks.length }} </span>
+                </v-fade-transition>
+            </h2>
+            <v-divider class="mt-4"></v-divider>
+            <v-row align="center" class="my-1">
+                <strong class="mx-4 text-info-darken-2"> Remaining: {{ remainingTasks }} </strong>
+                <v-divider vertical></v-divider>
+                <strong class="mx-4 text-success-darken-2"> Completed: {{ completedTasks }} </strong>
+                <v-spacer></v-spacer>
+                <v-progress-circular v-model="progress" class="me-2"></v-progress-circular>
+            </v-row>
+            <v-divider class="mb-4"></v-divider>
+            <v-card v-if="tasks.length > 0">
+                <v-slide-y-transition class="py-0" tag="v-list" group>
+                    <template v-for="(task, i) in tasks" :key="`${i}-${task.text}`">
+                        <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
+                        <v-list-item @click="toggleTaskDone(task)">
+                            <template v-slot:prepend>
+                                <v-checkbox-btn v-model="task.done" color="grey"></v-checkbox-btn>
+                            </template>
+                            <v-list-item-title>
+                                <span :class="task.done ? 'text-grey' : 'text-primary'">{{ task.text }}</span>
+                            </v-list-item-title>
+                            <template v-slot:append>
+                                <v-expand-x-transition>
+                                    <v-icon v-if="task.done" color="success"> mdi-check </v-icon>
+                                </v-expand-x-transition>
+                            </template>
+                        </v-list-item>
+                    </template>
+                </v-slide-y-transition>
+            </v-card>
+        </v-container>
     </v-app>
 </template>
 <script setup>
+import { ref, computed } from 'vue';
+
+// 响应式数据
+const tasks = ref([
+    {
+        done: false,
+        text: 'Foobar',
+    },
+    {
+        done: false,
+        text: 'Fizzbuzz',
+    },
+]);
+const newTask = ref(null);
+
+// 计算属性
+const completedTasks = computed(() => tasks.value.filter(task => task.done).length);
+const progress = computed(() => (completedTasks.value / tasks.value.length) * 100);
+const remainingTasks = computed(() => tasks.value.length - completedTasks.value);
+
+// 方法
+const create = () => {
+    tasks.value.push({
+        done: false,
+        text: newTask.value,
+    });
+    newTask.value = null;
+};
+
+const toggleTaskDone = (task) => {
+    task.done = !task.done;
+};
 
 import { useTheme } from 'vuetify'
 
 const theme = useTheme()
+
 function toggleTheme() {
     theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
 }
